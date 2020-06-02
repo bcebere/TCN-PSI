a0d7a653aae68fa7461cd3776ebe2a3cd9d3c57a
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-06-02 13:37:21 +0300

    raw PSI integration

--- tcn_psi/go/client/client.go
+++ tcn_psi/go/client/client.go
@@ -1,14 +1,50 @@
 package client
 
 import (
-	"fmt"
-	"github.com/openmined/psi/client"
+	"errors"
+	psiclient "github.com/openmined/psi/client"
 )
 
-func Client() {
-	psiClient, err := client.Create()
-	if err == nil {
-		fmt.Println("client loaded")
-		psiClient.Destroy()
+//TCNClient context for the client side of a TCN-Private Set Intersection-Cardinality protocol.
+type TCNClient struct {
+	context *psiclient.PsiClient
+}
+
+//Create returns a new TCN-PSI client
+func Create() (*TCNClient, error) {
+	tcnClient := new(TCNClient)
+
+	psiClient, err := psiclient.Create()
+	if err != nil {
+		return nil, err
 	}
+	tcnClient.context = psiClient
+	return tcnClient, nil
+}
+
+//CreateRequest generates a request message to be sent to the server.
+//
+//Returns an error if the context is invalid or if the encryption fails.
+func (c *TCNClient) CreateRequest(rawInput []string) (string, error) {
+	if c.context == nil {
+		return "", errors.New("invalid context")
+	}
+
+	return c.context.CreateRequest(rawInput)
+}
+
+//ProcessResponse processes the server's response and returns the PSI cardinality.
+//
+//Returns an error if the context is invalid,  if any input messages are malformed or if decryption fails.
+func (c *TCNClient) ProcessResponse(serverSetup, serverResponse string) (int64, error) {
+	if c.context == nil {
+		return 0, errors.New("invalid context")
+	}
+
+	return c.context.ProcessResponse(serverSetup, serverResponse)
+}
+
+//Version of the library.
+func (c *TCNClient) Version() string {
+	return c.context.Version()
 }
--- tcn_psi/go/server/server.go
+++ tcn_psi/go/server/server.go
@@ -1,14 +1,77 @@
 package server
 
 import (
-	"fmt"
-	"github.com/openmined/psi/server"
+	"errors"
+	psiserver "github.com/openmined/psi/server"
 )
 
-func Server() {
-	psiServer, err := server.CreateWithNewKey()
-	if err == nil {
-		fmt.Println("server loaded")
-		psiServer.Destroy()
+//TCNServer context for the server side of a TCN-Private Set Intersection-Cardinality protocol.
+type TCNServer struct {
+	context *psiserver.PsiServer
+}
+
+//CreateWithNewKey creates and returns a new server instance with a fresh private key.
+//
+//Returns an error if any crypto operations fail.
+func CreateWithNewKey() (*TCNServer, error) {
+	tcnServer := new(TCNServer)
+
+	psiServer, err := psiserver.CreateWithNewKey()
+	if err != nil {
+		return nil, err
 	}
+	tcnServer.context = psiServer
+	return tcnServer, nil
+}
+
+//CreateFromKey creates and returns a new server instance with the provided private key.
+//
+//Returns an error if any crypto operations fail.
+func CreateFromKey(key []byte) (*TCNServer, error) {
+	tcnServer := new(TCNServer)
+
+	psiServer, err := psiserver.CreateFromKey(key)
+	if err != nil {
+		return nil, err
+	}
+	tcnServer.context = psiServer
+	return tcnServer, nil
+}
+
+//CreateSetupMessage creates a setup message from the server's dataset to be sent to the
+//client.
+//
+//Returns an error if the context is invalid or if the encryption fails.
+func (s *TCNServer) CreateSetupMessage(fpr float64, inputCount int64, rawInput []string) (string, error) {
+	if s.context == nil {
+		return "", errors.New("invalid context")
+	}
+
+	return s.context.CreateSetupMessage(fpr, inputCount, rawInput)
+}
+
+//ProcessRequest processes a client query and returns the corresponding server response to
+//be sent to the client.
+//
+//Returns an error if the context is invalid.
+func (s *TCNServer) ProcessRequest(request string) (string, error) {
+	if s.context == nil {
+		return "", errors.New("invalid context")
+	}
+	return s.context.ProcessRequest(request)
+}
+
+//GetPrivateKeyBytes returns this instance's private key. This key should only be used to
+//create other server instances. DO NOT SEND THIS KEY TO ANY OTHER PARTY!
+func (s *TCNServer) GetPrivateKeyBytes() ([]byte, error) {
+	if s.context == nil {
+		return nil, errors.New("invalid context")
+	}
+
+	return s.context.GetPrivateKeyBytes()
+}
+
+//Version of the library.
+func (s *TCNServer) Version() string {
+	return s.context.Version()
 }
